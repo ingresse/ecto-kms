8ac3f702cf5939d872ea7d5c7c87fc2e06177d4c
Leoni Mella <leoni.mella@gmail.com>
2019-07-31 12:02:23 -0300

    Downgrade elixir version 1.9 -> 1.7.3

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Ecto.Kms.MixProject do
     [
       app: :ecto_kms,
       version: "1.0.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.7.3",
       start_permanent: Mix.env() == :prod,
       build_embedded: Mix.env() == :prod,
       deps: deps(),
